442e6d39480a0a30663efdf2293003132cb745ea
maryam-alizadeh <maryamaliizadeh1999@gmail.com>
2024-12-23 12:17:57 +0330

    WIP

--- src/Components/SearchBox.jsx
+++ src/Components/SearchBox.jsx
@@ -1,7 +1,8 @@
 import { IoSearch } from "react-icons/io5";
+import styles from "./SearchBox.module.css"
 function SearchBox({ search, setSearch, searchHandler }) {
   return (
-    <div>
+    <div className={styles.search}>
       <input type="text" placeholder="Search title" value={search} onChange={(e) => setSearch(e.target.value.toLowerCase().trim())} />
       <button onClick={searchHandler}>
         <IoSearch />
